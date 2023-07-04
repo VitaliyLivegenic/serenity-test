69ecc87dc9bd692b6e25909704a91c47d93d4d68
Vitaliy Teslyuk <116560306+VitaliyLivegenic@users.noreply.github.com>
2023-07-04 12:10:07 +0300

    Update NavigateActions.java

--- src/test/java/starter/wikipedia/NavigateActions.java
+++ src/test/java/starter/wikipedia/NavigateActions.java
@@ -4,7 +4,7 @@ import net.serenitybdd.core.steps.UIInteractions;
 import net.thucydides.core.annotations.Step;
 
 /**
- * UIInteractionSteps let us define action classes which regroup related actions.
+ * UIInteractionSteps let us define action classes which regroup related actions...
  * The @Step annotation is used to indicate that this action will appear as a step in the reports.
  */
 public class NavigateActions extends UIInteractions {
